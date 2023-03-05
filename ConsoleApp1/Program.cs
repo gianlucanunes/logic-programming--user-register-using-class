1698b1cea2dbb57d9c6192d4be5b51934fb5b720
Gianluca <gianluca.nulima@gmail.com>
2023-03-05 18:03:01 -0300

    version 1.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,86 +7,59 @@
  */
 
 
-Usuario user = new Usuario();
-List<string> info = new List<string>();
+List<Usuario> info = new List<Usuario>();
 
-int controle1 = 0;
-int controle2 = 1;
+string opcIn = "S";
 
 
 Console.WriteLine("\n------------------\n|    Registro    |\n------------------\n");
 
 // Loop while para o usuário registrar mais de um registro caso ele queira.
-while (controle1 < controle2)
+while (opcIn == "S")
 {
 
 // Pedindo ao usuário para que este preencha as informações.
-_reg:
+    _reg:
+    Usuario obj = new Usuario();
     Console.Write("\nPreencha as informações abaixo para completar o registro.\n");
     Console.Write("Nome: ");
-    user.nome = Console.ReadLine();
+    obj.nome = Console.ReadLine();
 
     Console.Write("Sobrenome: ");
-    user.sobrenome = Console.ReadLine();
+    obj.sobrenome = Console.ReadLine();
 
+    _idade:
     Console.Write("Idade: ");
-    user.idade = Console.ReadLine();
 
+    int id;
+    if (int.TryParse(Console.ReadLine(), out id))
+        obj.idade = id;
+    else
+    {
+        Console.WriteLine("\nValor inválido! Tente novamente!\n");
+        goto _idade;
+    }
+
+    _senha:
     Console.Write("Senha (6 dígitos): ");
-    user.senha = Console.ReadLine();
+    obj.senha = Console.ReadLine();
 
-
-    // Métodos para verificar a Idade e a Senha
-    int VerificaIdade()
+    if (obj.senha.Length != 6)
     {
-        try
-        {
-            Convert.ToInt32(user.idade);
-            return 1; ;
-        }
-        catch
-        {
-            return 0;
-        }
-    }
-
-    int VerificaSenha()
-    {
-        if ((user.senha).Length != 6)
-            return 0;
-        else
-            return 1;
+        Console.WriteLine("\nA senha não possui 6 dígitos! Tente novamente!\n");
+        goto _senha;
     }
 
 
-    // Verifica se a senha e/ou a idade fornecidas são válidas.
-    if (VerificaIdade() == 0 && VerificaSenha() == 1)
-    {
-        Console.WriteLine("\nERRO 001: A idade fornecida não é um valor válido! Tente novamente.\n");
-        goto _reg;
-    }
-    else if (VerificaIdade() == 1 && VerificaSenha() == 0)
-    {
-        Console.WriteLine("\nERRO 002: A senha fornecida não possui 6 dígitos! Tente novamente.\n");
-        goto _reg;
-    }
-    else if (VerificaIdade() == 0 && VerificaSenha() == 0)
-    {
-        Console.WriteLine("\nERRO 003: A senha não possui 6 dígitos e a idade fornecida não é um valor válido! Tente novamente.\n");
-        goto _reg;
-    }
 
 
     // Caso tudo esteja certo, armazena as informações dentro da lista.
-    info.Add(user.nome);
-    info.Add(user.sobrenome);
-    info.Add(user.idade);
-    info.Add(user.senha);
+    info.Add(obj);
     Console.WriteLine("\nUsuário cadastrado com sucesso!\n");
 
 
 // Confere se o usuário deseja adicionar um novo registro. Caso sim, ele é mandado para o início onde deverá preencher novas informações.
-_addReg:
+    _addReg:
     Console.WriteLine("\nDeseja adicionar um novo registro?\n[S] Sim\n[N] Não\n");
     string opc = Console.ReadLine().ToUpper();
 
@@ -95,36 +68,47 @@ _addReg:
         Console.WriteLine("\nERRO 004: Opção escolhida é inválida! Tente novamente!");
         goto _addReg;
     }
+
     else if (opc == "S")
+        goto _reg;
+
+    else
     {
-        controle1++;
-        controle2++;
+        opcIn = "N";
+        // Calcula o total de registros.
+        int totalReg = info.Count();
+
+        Console.WriteLine($"\nA coleção possui um total de {totalReg} registros.\n");
+
+
+    // Exibe ou não os dados para o usuário.
+    _listarReg:
+        Console.WriteLine("\nDeseja listar os registros?\n[S] Sim \n[N] Não");
+        string opcListar = Console.ReadLine().ToUpper();
+
+        if (opcListar != "S" && opcListar != "N")
+        {
+            Console.WriteLine("\nERRO 004: Opção escolhida é inválida! Tente novamente!");
+            goto _listarReg;
+        }
+
+        else if (opcListar == "S")
+        {
+            foreach (var item in info)
+            {
+                Console.WriteLine(item.ExibirDados());
+            }
+        }
+
+        break;
     }
-    else break;
 
 }
 
-
-// Calcula o total de registros. Por que dividir por 4? Porque na lista, a cada 4 posições há um novo registro. Logo, quando você divide por 4, você calcula o total de registros existentes dentro da lista.
-int totalReg = info.Count() / 4;
-
-Console.WriteLine($"\nA coleção possui um total de {totalReg} registros.\n");
+Console.WriteLine("\nTecle espaço para encerrar...");
+Console.ReadKey();
 
 
-// Exibe ou não os dados para o usuário.
-if (user.ExibirDados() == 1)
-{
-    Console.WriteLine();
-    for (int posicao = 0; posicao < info.Count(); posicao = posicao + 4)
-    {
-        Console.WriteLine($"Nome: {info[posicao]} {info[posicao + 1]} - Idade: {info[posicao + 2]} - Senha: {info[posicao + 3]}");
-    }
-}
-else
-{
-    Console.WriteLine("\nTecle espaço para encerrar...");
-    Console.ReadKey();
-}
 
 
 
--- ConsoleApp1/Usuario.cs
+++ ConsoleApp1/Usuario.cs
@@ -3,24 +3,12 @@
 
     public string nome { get; set; }
     public string sobrenome { get; set; }
-    public string idade { get; set; }
+    public int idade { get; set; }
     public string senha { get; set; }
 
-    public int ExibirDados()
+    public string ExibirDados()
     {
-    _listarReg:
-        Console.WriteLine("\nDeseja listar os registros?\n[S] Sim \n[N] Não");
-        string opcListar = Console.ReadLine().ToUpper();
-
-        if (opcListar != "S" && opcListar != "N")
-        {
-            Console.WriteLine("\nERRO 004: Opção escolhida é inválida! Tente novamente!");
-            goto _listarReg;
-        }
-        else if (opcListar == "N")
-            return 0;
-        else
-            return 1;
+        return $"Nome: {nome} {sobrenome} - Idade: {idade} anos - Senha: {senha}";
     }
 
 }
